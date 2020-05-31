adbbd9d2ea616de9b98f881b9ba8414b8079021d
SarthakKuchhal <sarthak@credochain.com>
2020-05-31 13:55:31 +0530

    modified:   .gitignore modified:   src/Components/Directory/Directory.js 	modified:   src/Components/MenuItem/MenuItem.js 	modified:   src/assets/scss/Stylesheets/_homepage.scss 	modified:   src/assets/scss/main.css

--- src/Components/MenuItem/MenuItem.js
+++ src/Components/MenuItem/MenuItem.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-export default function MenuItem({ title,imageUrl }) {
+export default function MenuItem({ title, imageUrl, size }) {
   return (
-    <div className="menu-item" style={{
-        backgroundImage: `url(${imageUrl})`
-    }}>
+    <div className={`menu-item ${size}`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">Shop Now</span>
       </div>
     </div>
