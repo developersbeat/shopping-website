2adfd7263bf1422798ccdda9fd884b22f8b5904d
SarthakKuchhal <sarthak@credochain.com>
2020-07-02 16:04:43 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './assets/scss/main.css';
 import Homepage from './Views/Homepage';
 import ShopPage from "./Views/Shop/shop";
 import {Route, Switch} from 'react-router-dom';
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <Fragment>
+    <Header/>
      <Switch>
        <Route exact path='/' component={Homepage}/>
        <Route exact path='/shop' component={ShopPage}/>
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {ReactComponent as Logo } from '../../assets/images/crown.svg'
+function header() {
+    return (
+        <div className="header">
+            <Link to="/" className="logo-container">
+                <Logo className="logo" />
+            </Link>
+            <div className="options">
+                <Link to="/shop" className="option">SHOP</Link>
+                <Link to="/shop" className="option">CONTACT</Link>
+
+            </div>
+        </div>
+    )
+}
+
+export default header
